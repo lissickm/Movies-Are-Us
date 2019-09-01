5de56319191ef2128260fdb471f0b8a11f196795
Matt Lissick <mattlissick@gmail.com>
2019-09-01 13:17:37 -0500

    the edit inputs are now changing the local state

--- src/components/MovieEdit/MovieEdit.jsx
+++ src/components/MovieEdit/MovieEdit.jsx
@@ -3,14 +3,29 @@ import { connect } from 'react-redux';
 
 class MovieEdit extends Component {
 
+    state = {
+        id: 0,
+        title: '',
+        description: ''
+    }
+
     handleCancelButton = (id) => {
         console.log('in handle cancel button');
         this.props.history.push('/details');
     }
 
-    handleSaveButton = (id) => {
-        console.log('in handle back button');
-        this.props.history.push('/details');
+    handleNewInputData = (propertyName, event) => {
+        console.log('in handle new input data');
+        this.setState({
+            ...this.state,
+            [propertyName]: event.target.value
+        })  
+    }
+
+    addNewMovieData = (event) => {
+        console.log('in add new movie data');
+        // this.props.history.push('/details');
+        
     }
 
     render() {
@@ -19,7 +34,17 @@ class MovieEdit extends Component {
             <div>
                 <p>In Movie Edit</p>
                 <button onClick={this.handleCancelButton}>Cancel</button>
-                <button onClick={this.handleSaveButton}>Save</button>
+                
+                <p>Enter the movie you wish to edit here</p>
+                <p>{JSON.stringify(this.state)}</p>
+                <form onSubmit={this.addNewMovieData}>
+                    
+                    <input type='text' placeholder='title' onChange={(event) => { this.handleNewInputData('title', event) }} />
+                    <input type='description' placeholder='description' onChange={(event) => { this.handleNewInputData('description', event) }} />
+                    <input type='submit' value='Save Edits' />
+                    
+                </form>
+                
             </div>
         )
 
